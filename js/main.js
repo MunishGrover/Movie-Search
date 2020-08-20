ba4e4bbf69ae95989df5754160271e908a11c7a7
MunishGrover <munishgrover98@gmail.com>
2020-08-20 13:34:52 +0530

    search bar

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  $("#searchForm").on("submit", e => {
+    // let searchText = $("#searchbar").val();
+    // getMovies(searchText);
+    // e.preventDefault();
+    alert("form submitted");
+  });
+});
